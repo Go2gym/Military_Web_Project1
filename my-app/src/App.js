441792985ca9fd971da23b385208415923ef744b
BubblingSODA <go2gym365@gmail.com>
2023-07-21 11:57:57 +0000

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
 
 function Hello() {
-  function byFn() {
-    consolee.log("bye :(");
-  }
-  function hiFn() {
-    console.log("Created :)");
-    return byFn;
-  }
-  useEffect(hiFn, [])
+  useEffect(() =>{
+    console.log("hi :)");
+    return () => console.log("bye :(");
+  }, []);
+  
+  useEffect(function(){
+    console.log("hi :)");
+    return function(){
+      console.log("bye :(")
+    }
+  }, []);
   return <h1>Hello</h1>;
 }
 
