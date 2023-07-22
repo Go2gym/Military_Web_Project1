6c4838fe56f172efed03dc2b4fa00db1d0cd3a21
BubblingSODA <go2gym365@gmail.com>
2023-07-22 14:25:59 +0000

    .

--- my-app/src/CoinTracker.js
+++ my-app/src/CoinTracker.js
@@ -2,8 +2,17 @@ import { useEffect, useState } from "react";
 import App from "./App";
 
 function CoinTracker() {
+    const [money, setMoney] = useState(1);
+    const [convert, setConvert] = useState(true);
     const [loading, setLoading] = useState(true);
     const [coins, setCoins] = useState([]);
+    const onChange = (event) => {
+        if (parseInt(event.target.value) === NaN) {
+            setMoney(1)
+            return
+        }
+        setMoney(parseInt(event.target.value));
+    };
     useEffect(() => {
         fetch("https://api.coinpaprika.com/v1/tickers")
             .then((response) => response.json())
@@ -12,21 +21,36 @@ function CoinTracker() {
                 setLoading(false);
             });
     }, []);
+
+    const onClick = () => {
+        setConvert((current) => !current);
+    }
     return (
         <div>
             <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+            <form>
+                <input
+                    onChange = {onChange}
+                    //value = {money}
+                    type="text"
+                    placeholder="write your money"
+                />
+                <button onClick={onClick}>{convert ? "convert" : "Turn Back"}</button>
+            </form >
             {loading ? (
                 <string>Loading... </string>
             ) : (
                 <select>
                     {coins.map((coin) => (
                         <option>
-                            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+                            {convert ? 
+                                `${coin.name} (${coin.symbol}): ${coin.quotes.USD.price} USD`
+                                : `${coin.name} (${coin.symbol}): ${Math.floor(money / coin.quotes.USD.price)} EA`
+                            }
                         </option>
                     ))}
                 </select>
             )}
-
         </div>
     );
 }
