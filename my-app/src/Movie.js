4ca93c351fe7a442924064b05b9a6024310b1d09
BubblingSODA <go2gym365@gmail.com>
2023-07-23 08:00:47 +0000

    ..

--- my-app/src/Movie.js
+++ my-app/src/Movie.js
@@ -1,34 +1,16 @@
-import { useEffect, useState } from "react";
-
-function Movie() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMovies = async () => {
-        const json = await (
-            await fetch(
-                'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
-            )
-        ).json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    }
-    useEffect(() => {
-        getMovies();
-    }, []);
-    return (
-        <div>
-            {loading ? (
-                <h1>Loading...</h1>
-            ) : (
-                <div>
-                    {movies.map(movie => (
-                        <div key={movie.id}>
-                            {movie.title}
-                        </div>
-                    ))}
-                </div>
-            )}
-        </div>
-    );
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt=""/>
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
 export default Movie;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import App2 from './App2';
 import CoinTracker from './CoinTracker';
-import Movie from './Movie';
+import App_Movie from './App_Movie';
 
 ReactDOM.render( 
-    <CoinTracker />,
+    <App_Movie />,
   document.getElementById('root')
 );
