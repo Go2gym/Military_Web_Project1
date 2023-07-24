8efd536d7776e76d8f86604243711b473fb89eb1
BubblingSODA <go2gym365@gmail.com>
2023-07-24 10:20:27 +0000

    .

--- my-app/src/routes/Home.js
+++ my-app/src/routes/Home.js
@@ -1,21 +1,26 @@
 import { useEffect, useState } from "react";
-import Movie from "../components/App_Movie";
+import Movie from "../Movie";
 
 function Home() {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
+
+  const getMovies = () => {
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+    ).then((v) => {
+      v.json().then((_v) => {
+        setLoading(false);
+        setMovies(_v.data.movies)
+        console.log(_v)
+      })
+    })
   };
+
   useEffect(() => {
     getMovies();
   }, []);
+
   return (
     <div>
       {loading ? (
